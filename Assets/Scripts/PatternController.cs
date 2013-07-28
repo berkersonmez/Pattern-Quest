706e4c15b75aaa154444bcf0f7efb37c83bdb46e
Berker Sönmez <brkrsnmz@gmail.com>
2013-07-28 21:30:54 +0300

    Bridges added to pattern drawing.

--- Assets/Scripts/PatternController.cs
+++ Assets/Scripts/PatternController.cs
@@ -7,8 +7,10 @@ public class PatternController : MonoBehaviour {
 	public static PatternController instance;
 	
 	public GameObject buttonPrefab;
+	public GameObject bridgePrefab;
 	public PatternButton[,] buttons = new PatternButton[4,4];
 	public List<int[]> pattern = new List<int[]>();
+	public List<GameObject> bridges = new List<GameObject>();
 	
 	void Start() {
 		instance = this;
@@ -40,11 +42,38 @@ public class PatternController : MonoBehaviour {
 				buttons[x,y].highlight();
 				int[] newNode = {x, y};
 				pattern.Add(newNode);
+				drawBridgeBetween(lastElement, newNode);
 			}
 		} else {
 			buttons[x,y].highlight();
 			int[] newNode = {x, y};
 			pattern.Add(newNode);
+		}
+	}
+	
+	public void drawBridgeBetween(int[] n1, int[] n2) {
+		tk2dCameraAnchor anchor = GameObject.Find("AnchorLL").GetComponent<tk2dCameraAnchor>();
+		GameObject obj = Instantiate(bridgePrefab) as GameObject;
+		obj.transform.parent = anchor.transform;
+		Vector3 n1Pos = buttons[n1[0], n1[1]].transform.localPosition;
+		Vector3 n2Pos = buttons[n2[0], n2[1]].transform.localPosition;
+		obj.transform.localPosition = (n1Pos + n2Pos) / 2;
+		obj.transform.localPosition += new Vector3(0, 0, 1f);
+		obj.GetComponent<tk2dSprite>().SetSprite("pattern_bridge_" + bridgeType(n1, n2));
+		bridges.Add(obj);
+	}
+	
+	public string bridgeType(int[] n1, int[] n2) {
+		int xd = n1[0] - n2[0];
+		int yd = n1[1] - n2[1];
+		if (xd == 0) {
+			return "v";
+		} else if (yd == 0) {
+			return "h";
+		} else if ((xd < 0 && yd < 0) || (xd > 0 && yd > 0)) {
+			return "d1";
+		} else {
+			return "d2";
 		}
 	}
 	
@@ -56,6 +85,10 @@ public class PatternController : MonoBehaviour {
 		}
 		Debug.Log("===============");
 		pattern.Clear();
+		foreach(GameObject bridge in bridges) {
+			Destroy(bridge);
+		}
+		bridges.Clear();
 	}
 	
 }
