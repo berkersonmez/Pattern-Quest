da63971fbd30b2035eea6eae2c94d22e7eec30f1
Oğuz Onur Kul <oguzkul@gmail.com>
2013-07-31 00:21:57 +0300

    xml

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Item : MonoBehaviour {
 	
-	int damage;
-	int spellPower;
-	string type;
-	int armor;
-	int hp;
-	int mana;
-	int level;
+	public string name;
+	public int damage;
+	public int spellPower;
+	public string type;
+	public int armor;
+	public int hp;
+	public int mana;
+	public int level;
+	
+	public Item(){
+		damage = 0;
+		spellPower = 0;
+		type = "common";
+		armor = 0;
+		hp = 0;
+		mana = 0;
+		level = 1;		
+	}
 	
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/XmlParse.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class XmlParse : MonoBehaviour {
+	
+	public List<Item> itemList = new List<Item>();
+	
+	public void getItem() {
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.LoadXml("item.xml");
+		XmlNodeList xmlItems = xmlDoc.GetElementsByTagName("item");
+		
+		foreach (XmlNode oneItem in xmlItems) {
+  			XmlNodeList itemContentList = oneItem.ChildNodes;
+  			Item obj = new Item(); 
+    
+   			foreach (XmlNode itemContent in itemContentList) {
+    			if(itemContent.Name == "name")
+    				obj.name = itemContent.InnerText;
+    			if(itemContent.Name == "damage")
+    				obj.damage = int.Parse(itemContent.InnerText);
+				if(itemContent.Name == "spellPower")
+    				obj.spellPower = int.Parse(itemContent.InnerText);
+				if(itemContent.Name == "type")
+    				obj.type = itemContent.InnerText;
+				if(itemContent.Name == "armor")
+    				obj.armor = int.Parse(itemContent.InnerText);
+				if(itemContent.Name == "hp")
+    				obj.hp = int.Parse(itemContent.InnerText);
+				if(itemContent.Name == "mana")
+    				obj.mana = int.Parse(itemContent.InnerText);
+				if(itemContent.Name == "level")
+    				obj.level = int.Parse(itemContent.InnerText);
+		   }
+		   itemList.Add(obj);
+			
+  		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		getItem ();
+		Debug.Log("(name:" + itemList[0].name + "damage:" + itemList[0].damage + ")");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
