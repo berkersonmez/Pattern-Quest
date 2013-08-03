58668e942cc8a18445a166faeac73bc9662999d4
Berker Sönmez <brkrsnmz@gmail.com>
2013-08-03 21:48:53 +0300

    GUI (Health/Mana bars)

--- Assets/Scripts/GUI/Healthbar.cs
+++ Assets/Scripts/GUI/Healthbar.cs
@@ -4,14 +4,18 @@ using System.Collections;
 public class Healthbar : MonoBehaviour {
 	public Creature owner;
 	private tk2dUIProgressBar progressBar;
+	private tk2dTextMesh text;
 	
 	void Start() {
 		progressBar = GetComponent<tk2dUIProgressBar>();
+		text = transform.Find("Background").Find("ProgressBarHighlight").Find("Text").GetComponent<tk2dTextMesh>();
 	}
 	
 	void Update () {
 		if (owner != null) {
 			progressBar.Value = (float)owner.currentHp / owner.hp;
+			text.text = owner.currentHp + "/" + owner.hp;
+			text.Commit();
 		}
 	}
 }
--- Assets/Scripts/GUI/Manabar.cs
+++ Assets/Scripts/GUI/Manabar.cs
@@ -5,14 +5,18 @@ public class Manabar : MonoBehaviour {
 
 	public Creature owner;
 	private tk2dUIProgressBar progressBar;
+	private tk2dTextMesh text;
 	
 	void Start() {
 		progressBar = GetComponent<tk2dUIProgressBar>();
+		text = transform.Find("Background").Find("ProgressBarHighlight").Find("Text").GetComponent<tk2dTextMesh>();
 	}
 	
 	void Update () {
 		if (owner != null) {
 			progressBar.Value = (float)owner.currentMana / owner.mana;
+			text.text = owner.currentMana + "/" + owner.mana;
+			text.Commit();
 		}
 	}
 }
