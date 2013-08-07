16f923c60333dc08a65d6cf0a5c0e638d429ffae
Oğuz Onur Kul <oguzkul@gmail.com>
2013-08-07 04:32:37 +0300

    Creature item basic

--- Assets/Scripts/XmlParse.cs
+++ Assets/Scripts/XmlParse.cs
@@ -111,6 +111,46 @@ public class XmlParse : MonoBehaviour {
 		
 	}
 	
+	public Item getItem(string name){
+		Item obj = new Item();
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.LoadXml(itemXML.text);
+		
+		XmlNode datNode = xmlDoc.SelectSingleNode("/items/item[name='" + name + "']");
+		XmlNodeList itemContentList = datNode.ChildNodes;
+		
+		foreach (XmlNode itemContent in itemContentList) {
+		if(itemContent.Name == "name"){
+			if(itemContent.InnerText != null)
+				obj.name = itemContent.InnerText;		
+		}
+		if(itemContent.Name == "damage"){
+			obj.damage = getValue(itemContent.InnerText);
+		}
+		if(itemContent.Name == "spellPower")
+			obj.spellPower = getValue(itemContent.InnerText);
+		if(itemContent.Name == "type"){
+			int typeCode = getValue(itemContent.InnerText);
+			obj.type = getType(typeCode);
+		}
+		if(itemContent.Name == "rarity")
+			obj.rarity = itemContent.InnerText;
+		if(itemContent.Name == "armor")
+			obj.armor = getValue(itemContent.InnerText);
+		if(itemContent.Name == "hp")
+			obj.hp = getValue(itemContent.InnerText);
+		if(itemContent.Name == "mana")
+			obj.mana = getValue(itemContent.InnerText);
+		if(itemContent.Name == "manaRegen")
+			obj.manaRegen = getValue(itemContent.InnerText);
+		if(itemContent.Name == "level")
+			obj.level = getValue(itemContent.InnerText);
+	  	}
+		//Fixes empty and wrong values
+		obj.setValues();
+	  	return obj;
+	}
+	
 	public void getItem() {
 		XmlDocument xmlDoc = new XmlDocument();
 		xmlDoc.LoadXml(itemXML.text);
