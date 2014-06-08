1e31be212b4dfcd3b2d2b8edde95856c99dddd08
Berker Sönmez <brkrsnmz@gmail.com>
2014-06-08 13:34:52 +0300

    Tutorial

---
+++ Assets/Scripts/GUI/TutorialWindow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialWindow : MonoBehaviour {
+	
+	void Start() {
+		transform.Find("CloseButton").GetComponent<tk2dUIItem>().OnClick += OnClose;
+		initialize();
+	}
+	
+	public void initialize() {
+		this.transform.parent = GameObject.Find("TutorialAnchor").transform;
+		this.transform.localPosition = new Vector3(0f, 0f, 0f);
+	}
+	
+	void OnClose() {
+		Destroy(this.gameObject);
+	}
+}
